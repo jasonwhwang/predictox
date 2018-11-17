1f4a4b4b5943810b75227910b0a0f27eb75ed12b
Jason Hwang <jasonwhwang@gmail.com>
2018-11-17 02:34:59 -0600

    Minor add change

--- src/views/Home/HomeComponents/Add.js
+++ src/views/Home/HomeComponents/Add.js
@@ -5,13 +5,17 @@ class Add extends React.Component {
   render() {
     return (
       <div className="h-modal box-flexRow">
-        <div className="h-modalContainer box-flexCol">
-          <button className="h-addDate box-text-2 box-textBold h-addMarginTopBig">November 7, 2018</button>
-          <input className="h-addHeight h-addMarginTopBig box-textCenter"></input>
-          <div className="h-addMarginTopSmall box-textBold box-textGray">Prediction</div>
-          <input className="h-addHeight h-addMarginTopBig box-textCenter"></input>
-          <div className="h-addMarginTopSmall box-textBold box-textGray">Stakes</div>
-          <button className="h-addMarginTopBig box-button-big h-addButton box-blue">Submit</button>
+        <div className="h-modalContainer box-flexCol h-flexCenter">
+          <button className="h-addDate box-text-2 box-textBold">November 7, 2018</button>
+          <div>
+            <input className="h-addHeight box-textCenter"></input>
+            <div className=" box-textBold box-textGray box-textCenter h-addMarginTopSmall">Prediction</div>
+          </div>
+          <div>
+            <input className="h-addHeight box-textCenter"></input>
+            <div className="box-textBold box-textGray box-textCenter h-addMarginTopSmall">Stakes</div>
+          </div>
+          <button className="box-button-big h-addButton box-blue">Submit</button>
         </div>
       </div>
     );
